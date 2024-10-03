f94a8f1ce0bcb6ce2345e02f36d6c474d8819495
GaneshGit240 <ganeshrallapati97@gmail.com>
2024-10-03 14:56:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Responsive Navigation Menu
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
